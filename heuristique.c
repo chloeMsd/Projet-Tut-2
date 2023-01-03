10e3bbe44481e29bdb77fddc50367c756af869ec
justin <justin.pinheiro@laposte.net>
2023-01-03 10:43:52 +0100

    heuristique debut

---
+++ heuristique.c
@@ -0,0 +1,12 @@
+#include "instance.h"
+
+//le tableau en entrée contient tous les indices des objets, triés selon un certain ordre
+int* obtenirSolutionDirecte(int* solutionOrdonnees, const struct Instance* instance)
+{
+    int* solutionDirecte = malloc(sizeof(solutionOrdonnees));
+
+    for (int i = 0; i < instance->nombre_objets; i++)
+    {
+        
+    }
+}
