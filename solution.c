99f5ee51538185aaea248a2e4a30daac70bf97a3
Chloé <chloe.mussard@etu.univ-tours.fr>
2023-01-19 23:55:07 +0100

    ajout commentaires

--- solution.c
+++ solution.c
@@ -3,7 +3,7 @@
 #include "instance.h"
 #include "parser.h"
 
-
+//Calcul la valeur du sac à partir d'une solution donnée
 int SolutionFonctionObjectif(struct Instance* instance, int* solution){
 	int valeur=0;
 	for (int i=0; i<instance->N; i++)
@@ -16,8 +16,9 @@ int SolutionFonctionObjectif(struct Instance* instance, int* solution){
 	return valeur;
 }
 
+//Calcul pour chaque dimension du sac les poids de la solution
 int* SolutionCalculDimension(struct Instance* instance, int* solution){
-	//on cree un tableau de M cases c'est le totale des poids pour chaque dimension
+	//on cree un tableau de M cases, ce sera le totale des poids pour chaque dimension
 	int *s=malloc(instance->M*sizeof(int));
 
 	for (size_t i = 0; i < instance->M; i++)
@@ -38,6 +39,7 @@ int* SolutionCalculDimension(struct Instance* instance, int* solution){
 	return s;
 }
 
+//Test si la solution donnée respecte bien les dimensions du sac en terme de poids maximum
 int SolutionTestFaisabilite(struct Instance* instance, int* solution){
 	//retourne 0 si c'est ok pour toutes les dimensions et retourne 1 sinon
 	//on cree un tableau avec M cases (1 case = 1 dimensions)
@@ -53,6 +55,7 @@ int SolutionTestFaisabilite(struct Instance* instance, int* solution){
 	return 0;
 }
 
+//Affiche la solution donnée si c'est une solution possible pour une certaine instance
 void SolutionAfficher(struct Instance* instance, int* solution){
 	if ( SolutionTestFaisabilite(instance, solution) ){
 		for (int i=0; i < instance->N; i++){
@@ -61,6 +64,7 @@ void SolutionAfficher(struct Instance* instance, int* solution){
 	}
 }
 
+//Ecris dans un fichier la solution donnée si c'est une solution possible pour une certaine instance
 void SolutionEcrire(struct Instance* instance, int* solution){
 	if (SolutionTestFaisabilite==0){
 		FILE *f=fopen("solution.txt","w+");
